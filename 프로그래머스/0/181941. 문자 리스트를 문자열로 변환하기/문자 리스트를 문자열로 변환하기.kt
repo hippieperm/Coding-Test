881f4c295403ec3a7e175fdc0364ac4599f05728
hippieperm <126477028+hippieperm@users.noreply.github.com>
2024-10-04 21:17:44 +0900

    [level 0] Title: 문자 리스트를 문자열로 변환하기, Time: 4.57 ms, Memory: 59.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(arr: Array<String>): String {
+        var answer = ""
+        for (arrs in arr) {
+            answer += arrs
+        }
+        return answer
+    }
+}
